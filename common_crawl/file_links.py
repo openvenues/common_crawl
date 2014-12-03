53668129f4eee3fa0dedda14bc5a2136e2a54c17
Al <albarrentine@gmail.com>
2014-12-03 12:56:07 -0500

    [fix] file links job reporting and options

--- common_crawl/file_links.py
+++ common_crawl/file_links.py
@@ -10,12 +10,12 @@ class FindFilesJob(CommonCrawlJob):
         super(FindFilesJob, self).configure_options()
         self.add_passthrough_option('--extensions', default='')
         self.add_passthrough_option('--pattern')
-        self.add_passthrough_option('--insensitive', default=None)
+        self.add_passthrough_option('--insensitive', action="store_true", default=False)
 
     def mapper_init(self):
         self.valid_extensions = set([s.strip('. ') for s in self.options.extensions.split(',')])
         if self.options.pattern:
-            regex_flags = re.I if self.options.insensitive is not None else None
+            regex_flags = re.I if self.options.insensitive else None
             self.valid_extension_pattern = re.compile(self.options.pattern, regex_flags)
         else:
             self.valid_extension_pattern = None
@@ -36,7 +36,8 @@ class FindFilesJob(CommonCrawlJob):
             if not norm_href:
                 continue
             extension = parse_file_ext_from_url(norm_href)
-            if extension and extension.lower() in self.valid_extensions:
+            if extension and extension[-1].lower() in self.valid_extensions or '.'.join(extension[-2:]).lower() in self.valid_extensions:
+                self.increment_counter('commoncrawl', 'links_found', 1)
                 yield norm_href, 1
 
     def reducer(self, key, values):
